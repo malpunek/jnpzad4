39d970f5f3d07498486ddb19f5269fec8a1b2664
Maciej Godek <maciejgodek@gmail.com>
2016-11-25 23:09:53 +0100

    Właściwie wszystko jest, kompiluje się i (chyba) działa.

---
+++ citizen.h
@@ -0,0 +1,72 @@
+#ifndef CITIZEN_H
+#define CITIZEN_H
+#include <cassert>
+#include <type_traits>
+
+template <typename T,
+         T ageLower,
+         T ageUpper,
+         bool canAttack,
+         typename TypeTest = typename std::enable_if<std::is_arithmetic<T>::value, T>::type>
+struct Citizen {
+    private:
+        T age_;
+        T health_;
+    public:
+        Citizen() = delete;
+        Citizen(T age, T health)
+            : age_(age)
+            , health_(health)
+        {
+            assert(age >= ageLower);
+            assert(age <= ageUpper);
+        }
+        T getHealth() const { return health_; }
+        T getAge() const { return age_; }
+        void takeDamage (T damage) {
+            if (damage > health_)
+                health_ -= health_;
+            else
+                health_ -= damage;
+        }
+};
+
+template <typename T, T ageLower, T ageUpper>
+struct Citizen<T, ageLower, ageUpper, true> {
+    private:
+        T age_;
+        T health_;
+        T attackPower_;
+    public:
+        Citizen() = delete;
+        Citizen(T age, T health, T attackPower)
+            : age_(age)
+            , health_(health)
+            , attackPower_(attackPower)
+        {
+            assert(age >= ageLower);
+            assert(age <= ageUpper);
+        }
+
+        T getHealth() const { return health_; }
+        T getAge() const { return age_; }
+        T getAttackPower() const { return attackPower_; }
+        void takeDamage (T damage) {
+            if (damage > health_)
+                health_ -= health_;
+            else
+                health_ -= damage;
+        }
+};
+
+template <typename T>
+using Adult = Citizen<T, 18, 100, false>;
+
+template <typename T>
+using Teenager = Citizen<T, 11, 17, false>;
+
+template <typename T>
+using Sheriff = Citizen<T, 18, 100, true>;
+#endif
+
+
---
+++ monster.h
@@ -0,0 +1,59 @@
+#ifndef MONSTER_H
+#define MONSTER_H
+#include <cassert>
+#include <typeinfo>
+#include <type_traits>
+#include "citizen.h"
+
+template<typename T,
+         typename TypeTest = typename std::enable_if<std::is_arithmetic<T>::value, T>::type>
+struct Monster {
+    private:
+        T health_;
+        T attackPower_;
+    public:
+        Monster() = delete;
+        Monster(T health, T attackPower)
+            : health_(health)
+            , attackPower_(attackPower)
+        {}
+
+        static const std::type_info& valueType;
+
+        T getHealth() const {
+            return health_;
+        }
+        T getAttackPower() const {
+            return attackPower_;
+        }
+        void takeDamage(T damage) {
+            if (damage > health_)
+                health_ -= health_;
+            else
+                health_ -= damage;
+        }
+};
+
+template<typename T, typename TypeTest>
+const std::type_info& Monster<T, TypeTest>::valueType = typeid(T);
+
+template<typename T>
+using Vampire = Monster<T>;
+
+template<typename T>
+using Zombie = Monster<T>;
+
+template<typename T>
+using Mummy = Monster<T>;
+
+template<typename M, typename U>
+void attack(M& monster, U& victim) {
+    victim.takeDamage(monster.getAttackPower());
+}
+
+template<typename M, typename T>
+void attack(M& monster, Sheriff<T>& victim) {
+    victim.takeDamage(monster.getAttackPower());
+    monster.takeDamage(victim.getAttackPower());
+}
+#endif
