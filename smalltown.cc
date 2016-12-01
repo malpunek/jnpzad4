8c84b744b574c23faf71e97dff5e0bcc83f5ab92
malpunek <malpunek@gmail.com>
2016-12-01 18:43:08 +0100

    Pierwsza działająca wersja

--- smalltown.cc
+++ smalltown.cc
@@ -4,6 +4,32 @@
 #include "monster.h"
 using namespace std;
 
+template<int n>
+class fib {
+	public:
+		const static long long val = fib<n-1>::val + fib<n-2>::val;
+		static bool isFib(int a){
+			if (a == val or fib<n-1>::isFib(a))
+				return true;
+			return false;
+		}
+};
+
+template <>
+class fib<0> {
+	public:
+		const static long long val = 0;
+};
+
+template <>
+class fib<1> {
+	public:
+		const static long long val = 1;
+		static bool isFib(int a){
+			return a == 1;
+		}
+};
+
 /*
 Klasa SmallTown<typename M, typename U, U t0, U t1, typename... C>, gdzie:
 M – typ potwora nawiedzającego miasto,
@@ -27,16 +53,38 @@ class SmallTown{
 		M_type monster;
 		U current_time = t0, max_time = t1;
 		
-		size_t countAliveCitizens(){
+		/*size_t countAliveCitizens(){
 			size_t wynik = 1;
-	/*		for(size_t i = 0; i < sizeof...(C); ++i)
+			for(size_t i = 0; i < sizeof...(C); ++i)
 				if (get<i>(citizens).isAlive())
-					wynik++;*/
+					wynik++;
 			return wynik;
-		}
+		}*/
 
 		bool isCityDead(){
-			return countAliveCitizens() == 0;
+			return countAliveCitizens(std::index_sequence_for<C...> {}) == 0;
+		}
+
+		template<typename T>
+		size_t isAlive(T a_guy){
+			if (a_guy.getHealth() == a_guy.getHealth() - a_guy.getHealth())
+				return 0;
+			return 1;
+		}
+
+		template<typename T>
+		size_t counter(T firster){
+			return isAlive(firster);
+		}
+
+		template<typename T, typename... Ts>
+		size_t counter(T firster, Ts... lasts){
+			return counter(lasts...) + isAlive(firster);
+		}
+
+		template<std::size_t... Is>
+		size_t countAliveCitizens(std::index_sequence<Is...>){
+			return counter(get<Is>(citizens)...);
 		}
 
 		template<typename T>
@@ -50,7 +98,6 @@ class SmallTown{
 			attacker(lasts...);
 		}
 
-			
 		template<std::size_t... Is>
 		void attackEveryone(std::index_sequence<Is...>){
 			attacker(get<Is>(citizens)...);
@@ -67,9 +114,11 @@ class SmallTown{
 			else if (isCityDead())
 			   printf("%s\n", MONSTER_WON);	   
 			else {
-				for (int i = 0; i < 80; i++)
-					if (current_time == U(fib<i>::val)) //@TODO CAST, MAX_FIB as const
-						attack_time = true;
+				if (fib<80>::isFib(current_time))
+					attack_time = true;
+			//	for (int i = 0; i < 80; i++)
+			//		if (current_time == U(fib<i>::val)) //@TODO CAST, MAX_FIB as const
+			//			attack_time = true;
 				if (attack_time)
 					attackEveryone(std::index_sequence_for<C...>{});
 		//			for(size_t i = 0; i < sizeof...(C); ++i)
@@ -83,7 +132,8 @@ class SmallTown{
 		}
 
 		tuple<string, typename M::valueType, size_t> getStatus(){
-			return make_tuple(monster.getName(), monster.getHealth(), countAliveCitizens());
+			return make_tuple(monster.getName(), monster.getHealth(),
+				   	countAliveCitizens(std::index_sequence_for<C...>{}));
 		}
 
 };
