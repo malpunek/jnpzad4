be98b853757d290cf5a8b096018da23c1143aa9e
malpunek <malpunek@gmail.com>
2016-12-01 18:45:16 +0100

    Drobna kosmetyka

--- smalltown.cc
+++ smalltown.cc
@@ -30,36 +30,20 @@ class fib<1> {
 		}
 };
 
-/*
-Klasa SmallTown<typename M, typename U, U t0, U t1, typename... C>, gdzie:
-M – typ potwora nawiedzającego miasto,
-U – typ przechowujący czas,
-t0 – czas startowy,
-t1 – czas końcowy, po którego przekroczeniu następuje wyzerowanie licznika,
-C... – typy mieszkańców.
-Należy sprawdzać poprawność parametrów t0 i t1 w czasie kompilacji.
-*/
-		constexpr const char CITIZENS_WON[] = "CITIZENS WON";
-		constexpr const char MONSTER_WON[] = "MONSTER WON";
-		constexpr const char DRAW[] = "DRAW";
+// @TODO Należy sprawdzać poprawność parametrów t0 i t1 w czasie kompilacji.
+
+constexpr const char CITIZENS_WON[] = "CITIZENS WON";
+constexpr const char MONSTER_WON[] = "MONSTER WON";
+constexpr const char DRAW[] = "DRAW";
 
 template<typename M, typename U, U t0, U t1, typename ... C>
 class SmallTown{
 	private:
-		bool attack_time;
 		typedef tuple<C...> C_type;
 		typedef M M_type;
 		C_type citizens;
 		M_type monster;
 		U current_time = t0, max_time = t1;
-		
-		/*size_t countAliveCitizens(){
-			size_t wynik = 1;
-			for(size_t i = 0; i < sizeof...(C); ++i)
-				if (get<i>(citizens).isAlive())
-					wynik++;
-			return wynik;
-		}*/
 
 		bool isCityDead(){
 			return countAliveCitizens(std::index_sequence_for<C...> {}) == 0;
@@ -105,8 +89,6 @@ class SmallTown{
 	public:
 		
 		SmallTown(M m, C... c) : citizens(c...), monster(m), attack_time(false){};
-		
-
 
 		void tick(U timeStep){
 			if (!monster.isAlive())
@@ -115,14 +97,7 @@ class SmallTown{
 			   printf("%s\n", MONSTER_WON);	   
 			else {
 				if (fib<80>::isFib(current_time))
-					attack_time = true;
-			//	for (int i = 0; i < 80; i++)
-			//		if (current_time == U(fib<i>::val)) //@TODO CAST, MAX_FIB as const
-			//			attack_time = true;
-				if (attack_time)
 					attackEveryone(std::index_sequence_for<C...>{});
-		//			for(size_t i = 0; i < sizeof...(C); ++i)
-		//				attack(monster, get<i>(citizens));
 				}
 			while (current_time + timeStep > max_time){
 				timeStep -= max_time - current_time;
