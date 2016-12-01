9750d877ce0c2e701b253f27588bb905d6e7a9e6
Maciej Godek <maciejgodek@gmail.com>
2016-12-01 20:15:56 +0100

    Drobne poprawki; styl

--- smalltown.cc
+++ smalltown.cc
@@ -4,7 +4,11 @@
 #include <cstddef>
 #include "citizen.h"
 #include "monster.h"
-using namespace std;
+
+constexpr const size_t FIBO_MAX = 40;
+const char* CITIZENS_WON = "CITIZENS WON";
+const char* MONSTER_WON = "MONSTER WON";
+const char* DRAW = "DRAW";
 
 template<size_t N>
 struct Fibo {
@@ -22,88 +26,83 @@ struct Fibo {
     }
 };
 
-// @TODO Należy sprawdzać poprawność parametrów t0 i t1 w czasie kompilacji.
-
-constexpr const char CITIZENS_WON[] = "CITIZENS WON";
-constexpr const char MONSTER_WON[] = "MONSTER WON";
-constexpr const char DRAW[] = "DRAW";
-constexpr const size_t FIB_MAX = 40;
-
 template<typename M, typename U, U t0, U t1, typename ... C>
 class SmallTown{
-	private:
-		typedef tuple<C...> C_type;
-		typedef M M_type;
-		C_type citizens;
-		M_type monster;
-		U current_time = t0, max_time = t1;
+    private:
+        typedef tuple<C...> C_type;
+        typedef M M_type;
+        C_type citizens;
+        M_type monster;
+        U current_time = t0, max_time = t1;
 
-		bool isCityDead(){
-			return countAliveCitizens(std::index_sequence_for<C...> {}) == 0;
-		}
+        bool isCityDead() {
+            return countAliveCitizens(std::index_sequence_for<C...> {}) == 0;
+        }
 
-		template<typename T>
-		size_t isAlive(T a_guy){
-			if (a_guy.getHealth() == a_guy.getHealth() - a_guy.getHealth())
-				return 0;
-			return 1;
-		}
+        template<typename T>
+        size_t isAlive(T a_guy) {
+            if (a_guy.getHealth() == a_guy.getHealth() - a_guy.getHealth())
+                return 0;
+            return 1;
+        }
 
-		template<typename T>
-		size_t counter(T firster){
-			return isAlive(firster);
-		}
+        template<typename T>
+        size_t counter(T firster) {
+            return isAlive(firster);
+        }
 
-		template<typename T, typename... Ts>
-		size_t counter(T firster, Ts... lasts){
-			return counter(lasts...) + isAlive(firster);
-		}
+        template<typename T, typename... Ts>
+        size_t counter(T firster, Ts... lasts) {
+            return counter(lasts...) + isAlive(firster);
+        }
 
-		template<std::size_t... Is>
-		size_t countAliveCitizens(std::index_sequence<Is...>){
-			return counter(get<Is>(citizens)...);
-		}
+        template<std::size_t... Is>
+        size_t countAliveCitizens(std::index_sequence<Is...>) {
+            return counter(get<Is>(citizens)...);
+        }
 
-		template<typename T>
-		void attacker(T firster){
-			attack(monster, firster);
-		}
-		
-		template<typename T, typename ... Ts>
-		void attacker(T firster, Ts ... lasts){
-			attack(monster, firster);
-			attacker(lasts...);
-		}
+        template<typename T>
+        void attacker(T firster) {
+            attack(monster, firster);
+        }
+        
+        template<typename T, typename ... Ts>
+        void attacker(T firster, Ts ... lasts) {
+            attack(monster, firster);
+            attacker(lasts...);
+        }
 
-		template<std::size_t... Is>
-		void attackEveryone(std::index_sequence<Is...>){
-			attacker(get<Is>(citizens)...);
-			}
-	public:
-		
-		SmallTown(M m, C... c) : citizens(c...), monster(m){};
+        template<std::size_t... Is>
+        void attackEveryone(std::index_sequence<Is...>) {
+            attacker(get<Is>(citizens)...);
+        }
+    public:
+        
+        SmallTown(M m, C... c) : citizens(c...), monster(m){};
 
-		void tick(U timeStep){
-            constexpr static auto fibo = Fibo<FIB_MAX>();
-			if (!monster.isAlive())
-				printf("%s\n", isCityDead() ? DRAW:CITIZENS_WON);
-			else if (isCityDead())
-			   printf("%s\n", MONSTER_WON);	   
-			else {
-				if (fibo.isFibo(current_time))
-					attackEveryone(std::index_sequence_for<C...>{});
+        void tick(U timeStep) {
+            constexpr static auto fibo = Fibo<FIBO_MAX>();
+
+            if (!monster.isAlive())
+                printf("%s\n", isCityDead() ? DRAW:CITIZENS_WON);
+            else if (isCityDead())
+               printf("%s\n", MONSTER_WON);    
+            else {
+                if (fibo.isFibo(current_time))
+                    attackEveryone(std::index_sequence_for<C...>{});
             }
-			while (current_time + timeStep > max_time){
-				timeStep -= max_time - current_time;
-				current_time = max_time - max_time;
-            }
-			current_time += timeStep;
-		}
 
-		tuple<string, typename M::valueType, size_t> getStatus(){
-			return make_tuple(monster.getName(), monster.getHealth(),
-				   	countAliveCitizens(std::index_sequence_for<C...>{}));
-		}
+            while (current_time + timeStep > max_time){
+                timeStep -= max_time - current_time;
+                current_time = max_time - max_time;
+            }
+            current_time += timeStep;
+        }
+
+        tuple<string, typename M::valueType, size_t> getStatus(){
+            return make_tuple(std::string(monster.getName()), monster.getHealth(),
+                    countAliveCitizens(std::index_sequence_for<C...>{}));
+        }
 
 };
 
